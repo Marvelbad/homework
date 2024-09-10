c82543fd243ba5655ae56cc2e8bee0cd82923fba
Marvelbad <badra.aqua@gmail.com>
2024-09-10 22:24:35 +0300

    in progress

--- src/Lessons.java
+++ src/Lessons.java
@@ -1,2 +1,12 @@
-public class Lessons {
+public class Replace {
+    public static void main(String[] args) {
+        String path = "/usr/java/jdk1.8/bin/";
+
+        String jdk13 = "jdk-13";
+        System.out.println(n);
+    }
+
+    public static String changePath(String path, String jdk13) {
+        int index = path.indexOf("jdk");
+    }
 }
