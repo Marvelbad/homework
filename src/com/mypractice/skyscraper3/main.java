0b4f2a5186fc00fb0a682b9c873def4172235c35
Marvelbad <badra.aqua@gmail.com>
2024-09-17 16:07:48 +0300

    Done

---
+++ src/com/mypractice/skyscraper3/main.java
@@ -0,0 +1,8 @@
+package com.mypractice.skyscraper3;
+
+public class main {
+    public static void main(String[] args) {
+        Skyscraper skyscraper = new Skyscraper();
+        Skyscraper skyscraperUnknown = new Skyscraper(50, "Неизвестно");
+    }
+}
