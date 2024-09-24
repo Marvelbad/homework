581f2e83c1b3132b84354a5fd7baa4f5bb9b94ce
Marvelbad <badra.aqua@gmail.com>
2024-09-24 18:24:49 +0300

    Done

---
+++ src/com/mypractice/Iphone/Iphone.java
@@ -0,0 +1,29 @@
+package com.mypractice.Iphone;
+
+
+public class Iphone {
+    private String model;
+    private String color;
+    private int price;
+
+    public Iphone(String model, String color, int price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Iphone iphone = (Iphone) object;
+        return price == iphone.price && model.equals(iphone.model) && color.equals(iphone.color);
+    }
+
+    public static void main(String[] args) {
+        Iphone iphone1 = new Iphone("X", "Black", 999);
+        Iphone iphone2 = new Iphone("X", "Black", 999);
+
+        System.out.println(iphone1.equals(iphone2));
+    }
+
+}
