af170714a68b8a5b90ddf8f23518b12bf370b637
Marvelbad <badra.aqua@gmail.com>
2024-09-26 01:29:17 +0300

    Done

--- src/com/mypractice/Object_class/Employee/Main.java
+++ src/com/mypractice/Object_class/Employee/Main.java
@@ -6,13 +6,16 @@ public class Main {
         Employee employee1 = new Employee("John", 246, "NYPD", 3000);
         Employee employee2 = new Employee("Alex", 350, "Office", 8000);
 
-        if (employee.hashCode() == employee1.hashCode()) {
-            System.out.println(employee.equals(employee1));
+        if (employee.hashCode() == employee1.hashCode() && employee.equals(employee1)) {
+            System.out.println("Employee and Employee1 are similar.");
+        }   else {
+            System.out.println("Not similar");
         }
-        if (employee.hashCode() == employee2.hashCode()) {
-            System.out.println(employee.equals(employee2));
-        } else {
-            System.out.println(false);
+
+        if (employee.hashCode() == employee2.hashCode() && employee.equals(employee2)) {
+            System.out.println("Employee and Employee2 are similar.");
+        }   else {
+            System.out.println("Not similar");
         }
     }
 }
