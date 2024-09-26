920e459b7ba05e728a3b1c372745b95c9938f9bc
Marvelbad <badra.aqua@gmail.com>
2024-09-27 00:38:12 +0300

    Done

--- src/com/mypractice/Object_class/Student/Student.java
+++ src/com/mypractice/Object_class/Student/Student.java
@@ -26,26 +26,26 @@ public class Student {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || !(obj instanceof Student)) return false;
+        if (this == obj) return true; //Соблюден принцип рефлексивности , так как обьект равен самому себе.
+        if (obj == null || !(obj instanceof Student)) return false; // Неравенство. Если переданный обьект null.
         Student student = (Student) obj;
         return name.equals(student.name) && id == student.id && university.equals(student.university);
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //Согласованность. Если два обьекта не изменяются то результат вызова equals() будет оставаться постоянным.
         Student student1 = new Student("Alex", 335, 25, "Developer", 2000, "Arizona");
         Student student2 = new Student("Alex", 335, 25, "Developer", 2000, "Arizona");
         Student student3 = new Student("John", 400, 25, "Developer", 2000, "Yells");
 
-        if (student1.hashCode() == student2.hashCode()) {
-            System.out.println(student1.equals(student2));
+        if (student1.hashCode() == student2.hashCode()) { //Контракт. Если равны по equals то hashcodes будут одинаковы.
+            System.out.println(student1.equals(student2)); //Симметричность. За счет сравнения полей id, university.
         } else {
             System.out.println("Not equal by hashCode");
         }
 
         if (student1.hashCode() == student3.hashCode()) {
-            System.out.println(student1.equals(student3));
+            System.out.println(student1.equals(student3)); //Транзитивность была бы соблюдена если поля были бы идентичны.
         } else {
             System.out.println("Not equal by hashCode");
         }
