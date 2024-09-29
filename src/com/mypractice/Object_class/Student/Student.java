916b08d62fea704500eee5d2d3ed1e6837fcf27a
Marvelbad <badra.aqua@gmail.com>
2024-09-29 19:03:16 +0300

    В методе equals использован утилитарный класс Object.equals

--- src/com/mypractice/Object_class/Student/Student.java
+++ src/com/mypractice/Object_class/Student/Student.java
@@ -5,35 +5,32 @@ import java.util.Objects;
 public class Student {
     private final String name;
     private final int id;
-    private final int gpa;
     private final String university;
 
-    public Student(String name, int id, int gpa, String university) {
+    public Student(String name, int id, String university) {
         this.name = name;
         this.id = id;
-        this.gpa = gpa;
         this.university = university;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, id, gpa, university);
+        return Objects.hash(name, id, university);
     }
 
     @Override
     public boolean equals(Object object) {
         if (this == object) return true;
-        if (object == null || !(object instanceof Student)) return false;
+        if (object == null || getClass() != object.getClass()) return false;
         Student student = (Student) object;
-        return name.equals(student.name) && id == student.id && gpa == student.gpa && university.equals(student.university);
+        return Objects.equals(name, student.name) && id == student.id && university.equals(student.university);
     }
 
 
     public static void main(String[] args) {
-        Student student1 = new Student("Alex", 335, 5, "Yells");
-        Student student2 = new Student("Alex", 335, 5, "Yells");
+        Student student1 = new Student("Alex", 335, "Yells");
+        Student student2 = new Student("Alex", 335, "Yells");
 
         System.out.println(student1.equals(student2));
     }
-
 }
