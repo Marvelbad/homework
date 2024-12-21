02b5c092627ec4fc5909b329a71523ec9e2f066c
Marvelbad <badra.aqua@gmail.com>
2024-12-21 22:44:00 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/BankAccount.java
+++ MyNewModule/PracticeModule/src/NewPractice/BankAccount.java
@@ -12,7 +12,9 @@ public class BankAccount {
     }
 
     public void withdraw(double amount) {
-
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
     }
 
     public double getBalance() {
