fae4cc2ab3464bd56043fbe7169c79d90887efe8
Marvelbad <badra.aqua@gmail.com>
2025-01-10 19:31:41 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/Solution.java
+++ MyNewModule/PracticeModule/src/NewPractice/Solution.java
@@ -1,21 +1,31 @@
 package NewPractice;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.awt.*;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-        String sourceFile = console.readLine();
-        FileInputStream stream = new FileInputStream(sourceFile);
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        List<Integer> data = new ArrayList<>();
-        while (!sourceFile.isEmpty()) {
+        Map<String, Person> personMap = new HashMap<>();
 
+        personMap.put("user", new Person.User());
+        personMap.put("loser", new Person.Loser());
+        personMap.put("coder", new Person.Coder());
+
+        while (true) {
+            String key = reader.readLine();
+
+            if (!(personMap.containsKey(key.toLowerCase()))) {
+                System.out.println("No such word/class here...");
+                break;
+            }
+            Person person = personMap.get(key.toLowerCase());
+            person.doWork();
         }
     }
 }
