b021c35b7742cdb7fbde1e997f43e89e9d199b85
Marvelbad <badra.aqua@gmail.com>
2025-01-11 22:22:24 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/Solution.java
+++ MyNewModule/PracticeModule/src/NewPractice/Solution.java
@@ -11,21 +11,48 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        Map<String, Person> personMap = new HashMap<>();
-
-        personMap.put("user", new Person.User());
-        personMap.put("loser", new Person.Loser());
-        personMap.put("coder", new Person.Coder());
-
-        while (true) {
-            String key = reader.readLine();
-
-            if (!(personMap.containsKey(key.toLowerCase()))) {
-                System.out.println("No such word/class here...");
-                break;
+        String key;
+        while ((key = reader.readLine()) != null) {
+            Movie movie = MovieFactory.getMovie(key);
+            if (movie == null) {
+                return;
             }
-            Person person = personMap.get(key.toLowerCase());
-            person.doWork();
+            System.out.println(movie.getClass().getSimpleName());
         }
+    }
+
+    static class MovieFactory {
+        private static final Map<String, Class<? extends Movie>> movieMap = new HashMap<>();
+
+        static {
+            movieMap.put("soapOpera", SoapOpera.class);
+            movieMap.put("thriller", Thriller.class);
+            movieMap.put("cartoon", Cartoon.class);
+        }
+
+        static Movie getMovie(String key) {
+            Class<? extends Movie> movieClass = movieMap.get(key);
+            if (movieClass != null) {
+                try {
+                    return movieClass.getDeclaredConstructor().newInstance();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+    }
+
+
+    static abstract class Movie {
+    }
+
+    static class SoapOpera extends Movie {
+    }
+
+    static class Cartoon extends Movie {
+    }
+
+    static class Thriller extends Movie {
     }
 }
