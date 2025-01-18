9261b7ba31ca0d313d474dc8a3c198b4b23c629b
Marvelbad <badra.aqua@gmail.com>
2025-01-18 15:48:44 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/Solution.java
+++ MyNewModule/PracticeModule/src/NewPractice/Solution.java
@@ -2,57 +2,50 @@ package NewPractice;
 
 import java.awt.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         String key;
-        while ((key = reader.readLine()) != null) {
-            Movie movie = MovieFactory.getMovie(key);
-            if (movie == null) {
-                return;
-            }
-            System.out.println(movie.getClass().getSimpleName());
-        }
-    }
-
-    static class MovieFactory {
-        private static final Map<String, Class<? extends Movie>> movieMap = new HashMap<>();
-
-        static {
-            movieMap.put("soapOpera", SoapOpera.class);
-            movieMap.put("thriller", Thriller.class);
-            movieMap.put("cartoon", Cartoon.class);
-        }
-
-        static Movie getMovie(String key) {
-            Class<? extends Movie> movieClass = movieMap.get(key);
-            if (movieClass != null) {
+        while (!(key = reader.readLine()).equalsIgnoreCase("exit")) {
+            if (key.matches("\\D+")) {
+                print(key);
+            } else {
                 try {
-                    return movieClass.getDeclaredConstructor().newInstance();
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    if (key.contains(".")) {
+                        print(Double.parseDouble(key));
+                    } else {
+                        int num = Integer.parseInt(key);
+                        if (num > 0 && num < 128) {
+                            print((short) num);
+                        } else {
+                            print(num);
+                        }
+                    }
+                } catch (NumberFormatException e) {
+                    print(key);
                 }
             }
-            return null;
         }
     }
 
-
-    static abstract class Movie {
+    public static void print(Double value) {
+        System.out.println("Это тип Double, значение " + value);
     }
 
-    static class SoapOpera extends Movie {
+    public static void print(String value) {
+        System.out.println("Это тип String, значение " + value);
     }
 
-    static class Cartoon extends Movie {
+    public static void print(short value) {
+        System.out.println("Это тип short, значение " + value);
     }
 
-    static class Thriller extends Movie {
+    public static void print(Integer value) {
+        System.out.println("Это тип Integer, значение " + value);
     }
 }
