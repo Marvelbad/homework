0951036641514b68cedb809ce01331b33f4698d3
Marvelbad <badra.aqua@gmail.com>
2025-01-18 19:08:02 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/Solution.java
+++ MyNewModule/PracticeModule/src/NewPractice/Solution.java
@@ -2,50 +2,28 @@ package NewPractice;
 
 import java.awt.*;
 import java.io.*;
+import java.math.BigDecimal;
 import java.util.*;
 import java.util.List;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        String key;
-        while (!(key = reader.readLine()).equalsIgnoreCase("exit")) {
-            if (key.matches("\\D+")) {
-                print(key);
-            } else {
-                try {
-                    if (key.contains(".")) {
-                        print(Double.parseDouble(key));
-                    } else {
-                        int num = Integer.parseInt(key);
-                        if (num > 0 && num < 128) {
-                            print((short) num);
-                        } else {
-                            print(num);
-                        }
-                    }
-                } catch (NumberFormatException e) {
-                    print(key);
-                }
-            }
+        int input = Integer.parseInt(reader.readLine());
+        reader.close();
+
+        System.out.println(factorial(input));
+    }
+
+    public static String factorial(int input) {
+        if (input < 0) return "0";
+        if (input == 0) return "1";
+        BigDecimal bd = new BigDecimal(1);
+        for (int i = 1; i <= input; i++) {
+            bd = bd.multiply(new BigDecimal(i));
         }
-    }
 
-    public static void print(Double value) {
-        System.out.println("Это тип Double, значение " + value);
-    }
-
-    public static void print(String value) {
-        System.out.println("Это тип String, значение " + value);
-    }
-
-    public static void print(short value) {
-        System.out.println("Это тип short, значение " + value);
-    }
-
-    public static void print(Integer value) {
-        System.out.println("Это тип Integer, значение " + value);
+        return bd.toString();
     }
 }
