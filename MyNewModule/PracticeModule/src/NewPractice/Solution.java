4695288cae61dd1a01810fcfb79b65ae484baa65
Marvelbad <badra.aqua@gmail.com>
2025-01-26 18:07:38 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/Solution.java
+++ MyNewModule/PracticeModule/src/NewPractice/Solution.java
@@ -1,29 +1,17 @@
 package NewPractice;
 
-import java.awt.*;
-import java.io.*;
-import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.*;
 import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        int input = Integer.parseInt(reader.readLine());
-        reader.close();
-
-        System.out.println(factorial(input));
-    }
-
-    public static String factorial(int input) {
-        if (input < 0) return "0";
-        if (input == 0) return "1";
-        BigDecimal bd = new BigDecimal(1);
-        for (int i = 1; i <= input; i++) {
-            bd = bd.multiply(new BigDecimal(i));
-        }
-
-        return bd.toString();
+        Printer printer = new Printer("Alex");
+        new Thread(printer).start();
+//        thread1.start();
     }
 }
