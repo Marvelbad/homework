9bb96fa665928c2e79c0fcd876ebbc175e894703
Marvelbad <badra.aqua@gmail.com>
2025-02-03 20:29:03 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/Solution.java
+++ MyNewModule/PracticeModule/src/NewPractice/Solution.java
@@ -11,7 +11,7 @@ import java.util.stream.Stream;
 public class Solution {
     public static void main(String[] args) throws Exception {
         Robot robot1 = new Robot("Alex", 30);
-        Robot robot2 = new Robot("Alex", 30);
+        Robot robot2 = new Robot("Alex", 20);
 
         System.out.println(robot1.equals(robot2));
         System.out.println(robot1.hashCode());
