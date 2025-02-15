1ae3686f1fab8c9db3ae30dcc4f9d18bdf96ca42
Marvelbad <badra.aqua@gmail.com>
2025-02-15 18:34:17 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/Solution.java
+++ MyNewModule/PracticeModule/src/NewPractice/Solution.java
@@ -1,7 +1,6 @@
 package NewPractice;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.BufferOverflowException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -17,18 +16,35 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        TreeSet<String> files = new TreeSet<String>();
+        TreeMap<Integer, String> files = new TreeMap<>();
 
         while (true) {
-            try {
-                String input = reader.readLine();
-                if (input.equalsIgnoreCase("end")) {
-                    break;
-                }
-                files.add(input);
-            } catch (Exception e) {
-                System.out.println("Error: " + e.getMessage());
-            }
+            String input = reader.readLine();
+            if (input.equalsIgnoreCase("end")) break;
+
+            String[] parts = input.split("\\.part");
+            int partNumber = Integer.parseInt(parts[1]);
+            files.put(partNumber, input);
+        }
+//        System.out.println(files);
+        String firstFile = files.firstEntry().getValue();//вернет пару ключ-значение с наименьш ключом и вытиянем значение
+        String outputFileName = firstFile.split("\\.part")[0]; //Берем все до ".part"
+
+        try (BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(outputFileName))) {
+            files.entrySet().stream()
+                    .map(entry -> entry.getValue()) //берем имена файлов
+                    .forEach(fileName -> {
+                                try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream())) {
+                                    byte[] buffer = new byte[1024];
+                                    int bytesRead;
+                                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                                        outputStream.write(buffer, 0, bytesRead);
+                                    }
+                                } catch (IOException e) {
+                                    throw new UncheckedIOException(e);
+                                }
+                            }
+                    );
         }
     }
 }
