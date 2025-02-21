608f4603189ffae58f32a82b8750979757a39c5e
Marvelbad <badra.aqua@gmail.com>
2025-02-21 23:10:49 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/Solution.java
+++ MyNewModule/PracticeModule/src/NewPractice/Solution.java
@@ -2,6 +2,7 @@ package NewPractice;
 
 import java.io.*;
 import java.nio.BufferOverflowException;
+import java.nio.ByteBuffer;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -16,47 +17,31 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         if (args == null || args.length == 0) return;
 
-        TreeMap<Integer, String> treeMap = new TreeMap<>();
+        Map<Integer, String> map = new LinkedHashMap<>();
 
         BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-
         String fileName = console.readLine();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            if (args[0].equals("-u")) {
-                String fileData;
-                while ((fileData = reader.readLine()) != null) {
-                    String currentId = fileData.substring(0, 8).trim();
-                    Integer id = Integer.parseInt(currentId);
+            String fileData;
+            while ((fileData = reader.readLine()) != null) {
+                String currentId = fileData.substring(0, 8).trim();
+                int id = Integer.parseInt(fileData);
 
-                    String productName = fileData.substring(8, 38).trim();
-                    String price = fileData.substring(38, 46).trim();
-                    String quantity = fileData.substring(46).trim();
+                String productName = fileData.substring(8, 38).trim();
+                String price = fileData.substring(38, 46).trim();
+                String quantity = fileData.substring(46).trim();
 
-
-                    if (currentId.equals(args[1])) {
-                        String updateLine = String.format("%-8s%-30s%-8s%-4s",
-                                args[1],
-                                args[2],
-                                args[3],
-                                args[4]);
-                        treeMap.put(id, updateLine);
-                    } else {
-                        treeMap.put(id, String.format("%-30s%-8s%-4s", productName, price, quantity));
-                    }
+                if (args[0].equals("-u") && currentId.equals(args[1])) {
+                    String updateLine = String.format("%-30s%-8s%-4s",
+                            args[2],
+                            args[3],
+                            args[4]);
+                    map.put(id, updateLine);
+                } else if (args[0].equals("-d") && currentId.equals(args[1])) {
+                    continue;
                 }
             }
         }
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-            for (Map.Entry<Integer, String> entry : treeMap.entrySet()) {
-                writer.write(String.format("%-8s%s\n", entry.getKey().toString(), entry.getValue()));
-            }
-        }
-
-        String id = args[1];
-        String productName = args[2];
-        String price = args[3];
-        String quantity = args[4];
     }
 }
