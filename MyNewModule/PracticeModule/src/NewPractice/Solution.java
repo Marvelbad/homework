f6ec982e187c30ede8790a31187e2ffc9b2197df
Marvelbad <badra.aqua@gmail.com>
2025-02-25 19:45:32 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/Solution.java
+++ MyNewModule/PracticeModule/src/NewPractice/Solution.java
@@ -1,39 +1,22 @@
 package NewPractice;
 
-import java.io.*;
-import java.nio.BufferOverflowException;
-import java.nio.ByteBuffer;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.*;
-import java.util.List;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class Solution {
-    public static final List<Person> PEOPLE = new ArrayList<Person>();
-
     public static void main(String[] args) throws Exception {
-        String path = "/Users/badribagateliya/IdeaProjects/Learning Project/MyNewModule/PracticeModule/src/NewPractice/textFile";
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String name = line.replaceAll("\\d", "").trim();
+        stringArray("Hello World and Java");
+    }
 
-                String date = line.replaceAll("\\D", "").replaceFirst("(\\d{2})(\\d{2})(\\d+)", "$1 $2 $3");
-                SimpleDateFormat dateFormat = new SimpleDateFormat("dd MM yyyy");
-                Date date1 = dateFormat.parse(date);
+    public static void stringArray(String word) {
+        if (word == null || word.isEmpty() || word.trim().isEmpty()) {
+            System.out.println("Wrong string");
+            return;
+        }
+        String[] arr = word.split(" ");
+        for (String s : arr) {
+            StringBuilder builder = new StringBuilder(s);
+            String result = builder.reverse().toString();
 
-                PEOPLE.add(new Person(name, date1));
-
-                PEOPLE.forEach(person -> System.out.println(person.getName() + " - " + person.getBirthDate()));
-
-//                System.out.println(name + " " + dateFormat.format(date1));
-            }
+            System.out.print(result + " ");
         }
     }
 }
