c8b4baefc4edfdc350864e50bf07c1a478ed6542
Marvelbad <badra.aqua@gmail.com>
2025-02-27 19:43:01 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/Test.java
+++ MyNewModule/PracticeModule/src/NewPractice/Test.java
@@ -1,6 +1,7 @@
 package NewPractice;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -9,9 +10,20 @@ public class Test {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        List<Integer> newList = numbers.stream()
+        List<Integer> newList1 = numbers.stream()
                 .filter(num -> num % 2 == 0)
                 .collect(Collectors.toList());
-        System.out.println(newList);
+        System.out.println(newList1);
+    }
+
+    public static void main2(String[] args) {
+        List<Integer> numList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        List<Integer> newList2 = numList.stream()
+                .filter(num -> num % 2 == 0)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+        System.out.println(newList2);
+
     }
 }
