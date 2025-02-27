d306fce3b516bfcce8a410f7498964baf5648913
Marvelbad <badra.aqua@gmail.com>
2025-02-27 18:37:18 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/Test.java
+++ MyNewModule/PracticeModule/src/NewPractice/Test.java
@@ -1,20 +1,14 @@
 package NewPractice;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Test {
     public static void main(String[] args) {
-        strLength("Hello World", 1, 4);
-    }
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-    public static void strLength(String str, int start, int finish) {
-        String subStr = str.substring(1, 5); // "ello"
-        StringBuilder builder = new StringBuilder(subStr);
-        String reversedStr = builder.reverse().toString();
-
-        String h = str.substring(0, start); // "H"
-        String lastPart = str.substring(finish + 1); // " World"
-
-        String finalStr = h + reversedStr + lastPart;
-
-        System.out.println(finalStr);
+        System.out.println(numbers.stream()
+                .filter(num -> num % 2 == 0).count());
     }
 }
