14c43fe89f702027a0341f4c1820af7b0f89cd18
Marvelbad <badra.aqua@gmail.com>
2025-02-27 20:35:21 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/Test.java
+++ MyNewModule/PracticeModule/src/NewPractice/Test.java
@@ -8,22 +8,34 @@ import java.util.stream.Stream;
 
 public class Test {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
-        List<Integer> newList1 = numbers.stream()
-                .filter(num -> num % 2 == 0)
-                .collect(Collectors.toList());
-        System.out.println(newList1);
-    }
-
-    public static void main2(String[] args) {
         List<Integer> numList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        List<Integer> newList2 = numList.stream()
+        int sum = numList.stream()
                 .filter(num -> num % 2 == 0)
                 .sorted(Comparator.reverseOrder())
-                .collect(Collectors.toList());
-        System.out.println(newList2);
-
+                .mapToInt(Integer::intValue)
+                .sum();
+        System.out.println(sum);
     }
+
+//    public static void main2(String[] args) {
+//        List<Integer> numList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+//
+//        List<Integer> newList2 = numList.stream()
+//                .filter(num -> num % 2 == 0)
+//                .sorted(Comparator.reverseOrder())
+//                .collect(Collectors.toList());
+//        System.out.println(newList2);
+//
+//    }
+
+
+//    public static void mai4(String[] args) {
+//        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+//
+//        List<Integer> newList1 = numbers.stream()
+//                .filter(num -> num % 2 == 0)
+//                .collect(Collectors.toList());
+//        System.out.println(newList1);
+//    }
 }
