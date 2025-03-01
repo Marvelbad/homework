d6ddf18eedfd16e9828ed599def1bedbe2d04c3f
Marvelbad <badra.aqua@gmail.com>
2025-03-01 21:40:11 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/Solution.java
+++ MyNewModule/PracticeModule/src/NewPractice/Solution.java
@@ -3,22 +3,5 @@ package NewPractice;
 public class Solution {
     public static void main(String[] args) throws Exception {
 
-        System.out.println(getWordsReverseInColumn("Hello my nice world"));
     }
-
-    public static String getWordsReverseInColumn(String str) {
-        if (str == null || str.isEmpty() || str.trim().isEmpty()) {
-            System.out.println("Wrong string");
-        }
-
-        String[] words = str.split(" ");
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < words.length; i++) {
-            String word = words[i];
-            StringBuilder builder = new StringBuilder(word);
-            result.append(builder.reverse().toString()).append("\n");
-        }
-        return result.toString().trim();
-    }
-
 }
