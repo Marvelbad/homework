6e1c3930252ee601d1c5f30bc96c9923845b9f6e
Marvelbad <badra.aqua@gmail.com>
2025-03-03 22:04:23 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/Test.java
+++ MyNewModule/PracticeModule/src/NewPractice/Test.java
@@ -1,40 +1,20 @@
 package NewPractice;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Test {
     public static void main(String[] args) {
-        List<Integer> numList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
-        int sum = numList.stream()
-                .filter(num -> num % 2 == 0)
-                .mapToInt(Integer::valueOf)
-                .sum();
-        System.out.println(sum);
+        maxCharIndex("aabbbcccc");
     }
 
-//    public static void main2(String[] args) {
-//        List<Integer> numList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-//
-//        List<Integer> newList2 = numList.stream()
-//                .filter(num -> num % 2 == 0)
-//                .sorted(Comparator.reverseOrder())
-//                .collect(Collectors.toList());
-//        System.out.println(newList2);
-//
-//    }
+    public static int maxCharIndex(String str) {
+        if (str == null|| str.trim().isEmpty()) {
+            return -1;
+        }
 
-
-//    public static void mai4(String[] args) {
-//        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-//
-//        List<Integer> newList1 = numbers.stream()
-//                .filter(num -> num % 2 == 0)
-//                .collect(Collectors.toList());
-//        System.out.println(newList1);
-//    }
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        for (Map.Entry<>)
+    }
 }
