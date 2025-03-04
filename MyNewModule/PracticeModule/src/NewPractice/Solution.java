2eb0ce8f8db90eac56e10ab87585462301148b29
Marvelbad <badra.aqua@gmail.com>
2025-03-04 18:05:52 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/Solution.java
+++ MyNewModule/PracticeModule/src/NewPractice/Solution.java
@@ -7,16 +7,34 @@ import java.util.stream.Stream;
 public class Solution {
     public static void main(String[] args) throws Exception {
         maxCharIndex("aabbbcccc");
+
     }
 
     public static int maxCharIndex(String str) {
-        if (str ==null || str.trim().isEmpty()) {
+        if (str == null || str.trim().isEmpty()) {
             return -1;
         }
 
-        Map<Character, Integer> charFrequency = new HashMap<>();
+        HashMap<Character, Integer> mapCount = new HashMap<>();
+
         for (char c : str.toCharArray()) {
-            charFrequency.put(c, charFrequency.getOrDefault(c, 0) +1);
+            mapCount.put(c, mapCount.getOrDefault(c, 0) +1);
         }
+
+        int maxFreq = 0;
+
+        for (Map.Entry<Character, Integer> entry : mapCount.entrySet()) {
+            if (entry.getValue() > maxFreq) {
+                maxFreq = entry.getValue();
+            }
+        }
+
+        for (int i =0; i < str.length(); i++) {
+            if (mapCount.get(str.charAt(i)) == maxFreq) {
+                return i;
+            }
+        }
+
+        return maxFreq;
     }
 }
