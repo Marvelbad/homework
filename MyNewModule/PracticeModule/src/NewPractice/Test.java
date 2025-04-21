ecef37d2ba69a1a85ce11cf282ee298f703fbf11
Marvelbad <badra.aqua@gmail.com>
2025-04-21 20:40:16 +0300

    Study

--- MyNewModule/PracticeModule/src/NewPractice/Test.java
+++ MyNewModule/PracticeModule/src/NewPractice/Test.java
@@ -1,48 +1,14 @@
 package Coding;
 
+import javax.xml.transform.SourceLocator;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Test {
-    public static Thread.UncaughtExceptionHandler handler = new OurUncaughtExceptionHandler();
-
     public static void main(String[] args) throws InterruptedException {
-        TestedThread commonThread = new TestedThread(handler);
-
-        Thread threadA = new Thread(commonThread, "Нить 1");
-        Thread threadB = new Thread(commonThread, "Нить 2");
-
-        threadA.start();
-        threadB.start();
-
-        threadA.interrupt();
-        threadB.interrupt();
     }
 
 
-
-
-    public static class TestedThread extends Thread {
-        public TestedThread(Thread.UncaughtExceptionHandler handler) {
-            setUncaughtExceptionHandler(handler);
-            start();
-        }
-
-        public void run() {
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException("Error message");
-            }
-        }
-    }
-
-    public static class OurUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
-        @Override
-        public void uncaughtException(Thread t, Throwable e) {
-            System.out.println(t.getName() + ": " + e.getMessage());
-        }
-    }
 }
---
+++ src/com/mypractice/TestCode/Project.java
@@ -0,0 +1,22 @@
+package com.mypractice.TestCode;
+
+public class Project {
+    private String name;
+    private String status;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
