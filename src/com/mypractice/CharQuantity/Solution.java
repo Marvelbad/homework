7fef47218adda9bf49a17c8effb0c8c0a1cfa370
Marvelbad <badra.aqua@gmail.com>
2025-04-24 06:46:09 +0300

    Study

--- src/com/mypractice/CharQuantity/Solution.java
+++ src/com/mypractice/CharQuantity/Solution.java
@@ -1,21 +1,24 @@
 package com.mypractice.CharQuantity;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        File path = new File("/Users/badribagateliya/IdeaProjects/Learning Project/src/com/mypractice/CharQuantity/txt");
+        File path = new File(args[0]);
         BufferedReader reader = new BufferedReader(new FileReader(path));
         String line;
 
         StringBuilder builder = new StringBuilder();
         while ((line = reader.readLine()) != null) {
-            builder.append(line).append("\n");
+            builder.append(line);
         }
         String allText = builder.toString();
-        int charsCount = allText.replaceAll(" ", "").length();
-        System.out.println(charsCount);
+        int allCharsCount = allText.length();
+        int allSpacesCount = allText.replaceAll("[^ ]", "").length();
+
+        double percentage = (double) allSpacesCount / allCharsCount * 100;
+
+        System.out.println("Symbols quantity: " + allCharsCount + "\n" + "Spaces quantity: " + allSpacesCount);
+        System.out.println("Spaces percentage: " + Math.round(percentage));
     }
 }
