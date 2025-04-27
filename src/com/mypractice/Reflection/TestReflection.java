af0d83e9e9758762576cfe36819dd7eb76c3cf7a
Marvelbad <badra.aqua@gmail.com>
2025-04-27 18:03:10 +0300

    Study

--- src/com/mypractice/Reflection/TestReflection.java
+++ src/com/mypractice/Reflection/TestReflection.java
@@ -1,11 +1,20 @@
 package com.mypractice.Reflection;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestReflection {
     public static void main(String[] args) throws ClassNotFoundException {
         Person person = new Person();
 
-        Class personClass = person.getClass();
-        Class personClass2 = Person.class;
-        Class personClass3 = Class.forName("Reflection.Person");
+        Class<? extends Person> personClass = person.getClass();
+        Class<Person> personClass2 = Person.class;
+        Class<?> personClass3 = Class.forName("com.mypractice.Reflection.Person");
+
+        Method[] methods = personClass.getMethods();
+        for (Method method : methods) {
+            System.out.println(method.getName() + ", "
+            + method.getReturnType() + ", " + Arrays.toString(method.getParameterTypes()));
+        }
     }
 }
