77ae12a8419ba5144c6e7ef8c591896ed1580528
Marvelbad <badra.aqua@gmail.com>
2025-05-10 22:25:46 +0300

    Study

---
+++ src/com/mypractice/Pattern/Solution.java
@@ -0,0 +1,15 @@
+package com.mypractice.Pattern;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+    public static void main(String[] args) {
+        String text = "Hello24, my name is Bad and i am 28";
+        Pattern pattern = Pattern.compile("(?<=\\s|^)\\d+(?=\\s|$)");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
