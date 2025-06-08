feb2627b1f18667d6501aa5c4f414bf3fc937bdc
Marvelbad <badra.aqua@gmail.com>
2025-06-08 21:35:13 +0300

    Study

--- src/com/mypractice/JustTempPackage/Solution.java
+++ src/com/mypractice/JustTempPackage/Solution.java
@@ -1,26 +1,25 @@
 package com.mypractice.JustTempPackage;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        Path sourceDirectory = Path.of(scanner.nextLine());
-        Path targetDirectory = Path.of(scanner.nextLine());
+        List<String> list = Arrays.asList("Hello", "World");
+        List<String> newArr = list.stream()
+                .filter(word -> word.length() > 3)
+                .toList();
 
-        try (DirectoryStream<Path> files = Files.newDirectoryStream(sourceDirectory)) {
-            for (Path path : files) {
-                if (Files.isRegularFile(path)) {
-                    Path resolve = targetDirectory.resolve(path.getFileName());
-                    Files.move(path,resolve);
-                }
-            }
-        }
+        System.out.println(newArr);
     }
 }
