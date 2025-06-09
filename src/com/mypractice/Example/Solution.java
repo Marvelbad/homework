0787986de03180d422f04781f50bdcd71576ff69
Marvelbad <badra.aqua@gmail.com>
2025-06-09 22:20:43 +0300

    Study

--- src/com/mypractice/Example/Solution.java
+++ src/com/mypractice/Example/Solution.java
@@ -1,19 +1,15 @@
 package com.mypractice.Example;
 
 import java.io.*;
+import java.util.List;
 import java.util.Scanner;
 
-public class Solution {
+public class Solution<T extends List<Solution.SomeClass<?>>> {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        String fileName = scanner.nextLine();
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-            while (true) {
-                String line = scanner.nextLine();
-                writer.write(line);
-                writer.newLine();
-                if (line.equals("exit")) break;
-            }
-        }
+
+    }
+
+    public static class SomeClass<T extends Number> {
+
     }
 }
